a2f6569efcd214165ba07f77aab6652dc4677ac1
Matthew Palmer <matthewandrewpalmer@outlook.com>
2020-10-22 09:41:31 +0100

    Initial React Commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import Header from "./Header";
-import BetaBanner from "./BetaBanner";
-import ExternalLink from "./ExternalLink";
+import Header from "./Components/Header";
+import BetaBanner from "./Components/BetaBanner";
+import ExternalLink from "./Components/ExternalLink";
+import {DefaultErrorBoundary} from "./Components/DefaultErrorBoundary";
+import {ErrorBoundary} from "./Components/ErrorBoundary";
+import {field_period_to_text} from "./Functions";
 
 
 interface State {
     list: string[]
+}
+
+interface ListItem {
+    name: string
+    link: string
 }
 
 
@@ -16,14 +24,15 @@ function App() {
     }, []);
 
 
-    let [list, setList] = useState<string[]>([]);
+    let [list, setList] = useState<any[]>([]);
 
     function getList() {
         fetch('/api/getList')
-            .then((r: any) => (
+            .then((r: Response) => (
                     r.json()
-                        .then((json: string[]) => {
+                        .then((json: ListItem[]) => {
                                 console.log("Set List")
+                                console.log(json)
                                 setList(json)
                             }
                         ).catch((error: any) => (
@@ -38,52 +47,70 @@ function App() {
 
 
     return (
-        <div className="App">
+        <>
             <BetaBanner/>
             <Header/>
             <div className="page__container container ">
                 <main id="main-content" className="page__main ">
-                    <h1>Title</h1>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
+                    <DefaultErrorBoundary>
 
-                    <table id="basic-table" className="table ">
-                        <caption className="table__caption">A basic table with a caption</caption>
-                        <thead className="table__head">
-                        <tr className="table__row">
-                            <th scope="col" className="table__header ">
-                                <span>Column A</span>
-                            </th>
-                            <th scope="col" className="table__header ">
-                                <span>Column B</span>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody className="table__body">
-                        {
-                            list && list.length > 0
-                                ?
-                                list.map((item) => {
-                                    return (
-                                        <tr className="table__row">
-                                            <td className="table__cell ">
-                                                {item}
+                        <h1>Title</h1>
+                        <p>
+                            Edit <code>src/App.tsx</code> and save to reload.
+                        </p>
+
+                        <table id="basic-table" className="table ">
+                            <caption className="table__caption">A basic table with a caption</caption>
+                            <ErrorBoundary errorMessageText={"Unable to load table correctly"}>
+                                <thead className="table__head">
+                                <tr className="table__row">
+                                    <th scope="col" className="table__header ">
+                                        <span>Column A</span>
+                                    </th>
+                                    <th scope="col" className="table__header ">
+                                        <span>Column B</span>
+                                    </th>
+                                    <th scope="col" className="table__header ">
+                                        <span>Column C</span>
+                                    </th>
+                                </tr>
+                                </thead>
+                                <tbody className="table__body">
+
+                                {
+                                    list && list.length > 0
+                                        ?
+                                        list.map((item: ListItem) => {
+                                            return (
+                                                <tr className="table__row" key={item.name}>
+                                                    <td className="table__cell ">
+                                                        {item.name}
+                                                    </td>
+                                                    <td className="table__cell ">
+                                                        {field_period_to_text(item.name)}
+                                                    </td>
+                                                    <td className="table__cell ">
+                                                        <ExternalLink item={item.name} link={"/" + item.link}/>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                        :
+                                        <tr>
+                                            <td className="table__cell " colSpan={2}>
+                                                No items found
                                             </td>
-                                            <td className="table__cell ">
-                                                <ExternalLink item={item} link={"/" + item}/>
-                                            </td>
+
                                         </tr>
-                                    )
-                                })
-                                :
-                                <p>No items found</p>
-                        }
-                        </tbody>
-                    </table>
+                                }
+
+                                </tbody>
+                            </ErrorBoundary>
+                        </table>
+                    </DefaultErrorBoundary>
                 </main>
             </div>
-        </div>
+        </>
     );
 }
 
--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
+    // var list = ["item1", "item2", "item3"];
+    let list = [{name: "2004", link: "/link"},{name: "2007", link: "/link"},{name: "2101", link: "/link"}];
+
     // var list = null
     res.json(list);
     console.log('Sent list of items');
